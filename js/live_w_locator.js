6b9b67b54e1122fb346a67637141478f953810e0
Peter Jozsef <j.peter@camelweb.com>
2019-12-29 17:55:51 +0200

    Adding vibration when barcode recognized

--- js/live_w_locator.js
+++ js/live_w_locator.js
@@ -283,16 +283,8 @@ $(function() {
 
     Quagga.onDetected(function(result) {
         var code = result.codeResult.code;
-
-        if (App.lastResult !== code) {
-            App.lastResult = code;
-            var $node = null, canvas = Quagga.canvas.dom.image;
-
-            $node = $('<li><div class="thumbnail"><div class="imgWrapper"><img /></div><div class="caption"><h4 class="code"></h4></div></div></li>');
-            $node.find("img").attr("src", canvas.toDataURL());
-            $node.find("h4.code").html(code);
-            $("#result_strip ul.thumbnails").prepend($node);
-        }
+        $("#result_strip .result_code").html(code);
+        navigator.vibrate(1000);
     });
 
 });
